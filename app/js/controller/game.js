0b46064d42caa0d051aa5e851ea344413290fa20
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2017-10-27 22:38:36 +0200

    Added the socket example code

--- app/js/controller/game.js
+++ app/js/controller/game.js
@@ -38,6 +38,30 @@ angular.module('wriggle').controller(
         $scope.init = function () {
             $log.log('GameController: init');
 
+            $scope.openSocket();
+        };
+
+        $scope.openSocket = function () {
+            var socket = io('wriggle-backend.herokuapp.com');
+            var myId = null;
+            socket.on('connect', function () {
+                myId = socket.id;
+                console.log('my id', myId);
+            });
+            socket.on('joined', function (data) {
+                if (myId === data.id) {
+                    console.log('i joined. my postion', data.position);
+                    return;
+                }
+                console.log('someone joined', data);
+            });
+            socket.on('left', function () {
+                console.log('someone left');
+            });
+            socket.on('tick', function () {
+                console.log('tick occured');
+            });
+
         };
 
         /**
